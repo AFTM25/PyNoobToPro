1dec8f66f109882977b08c0fa82d1a077ea1e30e
AFTM25 <adityapratamay07@gmail.com>
2025-02-13 22:20:15 +0700

    Manipulasi

--- pemahaman_list/manipulasi_list.py
+++ pemahaman_list/manipulasi_list.py
@@ -11,3 +11,21 @@ print(f'Data pertama adalah : {data1}')
 data_terakhir = data_buah1[-1]
 print(f'Data terakhir adalah : {data_terakhir}')
 
+# mengetahui panjang atau jumlah item pada list
+jumlah_data = len(data_buah1)
+print(f'Jumlah data pada list buah adalah {jumlah_data}\n')
+
+# Menambah item list dengan menggunakan insert(indeks, newItem)
+data_buah1.insert(0, 'Stroberi')
+print(f'Data buah setelah di insert = {data_buah1}\n')
+
+# Menambahkan item list dengan posisi terakhir
+data_buah1.append('Nanas')
+print(f'Data buah setelah di append = {data_buah1}\n')
+
+# Menambahkan data list dengan data list baru
+data_buah = ['Pepaya', 'Belimbing', 'Semangka']
+data_buah1.extend(data_buah)
+print(f'Data buah setelah di extend = {data_buah1}')
+print(f'Jumlah nya sekarang adalah {len(data_buah1)} item')
+
