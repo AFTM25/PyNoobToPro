e9b170b3d129a7d0060cc43e435f4747c962bafc
AFTM25 <152005300+AFTM25@users.noreply.github.com>
2026-03-17 19:51:14 +0700

    Delete opr_ directory

--- opr_/str_operator.py
+++
@@ -1,6 +0,0 @@
-# Membahas soal penggabungan string / concatenate 
-# dan juga operator arterisk (*)
-# contoh berikut menghasilkan gambar kotak persegi 
-print('+' + 10 * '-' + '+')
-print(('|' + " " * 10 + '|\n') * 5, end="")
-print('+' + 10 * '-' + '+')
