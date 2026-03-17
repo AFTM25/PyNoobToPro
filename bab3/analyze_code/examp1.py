fa9d7aae48f6e5a3cb22a07a02f39d7b002c05c8
AFTM25 <152005300+AFTM25@users.noreply.github.com>
2026-03-17 19:51:51 +0700

    Delete bab3 directory

--- bab3/analyze_code/examp1.py
+++
@@ -1,8 +0,0 @@
-# Membandingkan kedua bilangan apakah lebih besar 
-num1 = int(input('Masukkan bilangan pertama: '))
-num2 = int(input('Masukkan bilangan kedua: '))
-
-if num1 > num2 : large_number = num1 
-else: large_number = num2 
-
-print('Bilangan terbesar adalah: ', large_number)
--- bab3/lab/comparison_conditional_if/main.py
+++
@@ -1,9 +0,0 @@
-houseplants = input('Enter your houseplants are : ')
-if houseplants == 'spathiphyllum':
-    print('No, I want a big Spathiphyllum!')
-
-if houseplants == 'Spathiphyllum':
-    print('Yes - Spathiphyllum is the best plant ever!')
-    
-if houseplants  != 'spathiphyllum' and houseplants != 'Spathiphyllum':
-    print(f'Spathiphyllum! Not {houseplants}!')
